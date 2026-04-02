bee59565575507a184befe111f9515d6eabfc585
Parvez-31 <parveztarafder31@gmail.com>
2026-04-02 20:24:58 +0530

    refactor: comment out previous code and implement evenOdd function

--- script.js
+++ script.js
@@ -1,2 +1,46 @@
-const arr = [5, 8, [23, 50, [45, 100]]]
-console.log(arr[2][2][0])
+// const arr = [5, 8, [23, 50, [45, 100]]]
+// console.log(arr[2][2][0])
+
+// function add(a, b) {
+//    return a + b 
+// }
+// let result = add(3, 6)
+
+// function multiply(x) {
+//    console.log(x * 2) 
+// }
+
+// multiply(result)
+
+// function voter(age) {
+// if(age >= 18) {
+//     return "eligible for vote"
+// }else {
+//     return "not eligible for vote"
+// }
+// }
+// let result = voter( 12)
+// console.log(result)
+
+// function eligibility (age) {
+// if(age < 0) {
+//     console.log("invalid input")
+// }else if(age >= 18) {
+//     console.log("eligible for vote")
+// }else {
+//     console.log("not eligible for vote")
+// }
+// }
+// eligibility(12)
+// eligibility(20)
+// eligibility(22)
+// eligibility(-1)
+
+// function evenOdd(num) {
+//     if(num % 2 === 0) {
+//         console.log("even")
+//     } else{
+//         console.log("odd")
+//     } 
+// }
+// evenOdd()

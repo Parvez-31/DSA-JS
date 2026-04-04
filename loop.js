dbdfb0772c9c3ac82333760d14dc2c57dc907364
Parvez Hossien Tarafder <parveztarafder31@gmail.com>
2026-04-04 18:52:21 +0530

    add commented-out code examples for loops and array manipulation

---
+++ loop.js
@@ -0,0 +1,53 @@
+// for (let i = 0; i < 5; i++) {
+//   console.log("hello world");
+// }
+
+// for (let count = 3; count < 5; ++count) {
+//   console.log(count);
+// }
+
+// let x = 10;
+
+// let a = x++;
+// let b = ++x;
+
+// console.log(a); // 10
+// console.log(b); // 12
+// console.log(x); // 12
+
+// let a = 5;
+// let z = a++ + 6;
+// console.log(z);
+
+// let x = 5;
+
+// let y = x++ + ++x; // 5 + 7
+
+// console.log(y); // 11
+
+// for (let i = 2; i < 9; i = i + 2) {
+//   console.log("hello world " + i);
+// }
+
+// for (let i = 5; i > 0; i--) {
+//   console.log("hello world " + i);
+// }
+
+// for (let i = 1; i > 0; i++) {
+//   console.log("hello world " + i);
+// }
+
+// let arr = [2, 3, 4, 5, 7, 8];
+// console.log(arr[arr.length - 1]);
+
+// for (let i = 0; i < arr.length; i++) {
+//   if (arr[i] % 2 === 1) {
+//     console.log(arr[i]);
+//   }
+// }
+
+// let i = 0;
+// while (arr.length > i) {
+//   console.log(arr[i]);
+//   i++;
+// }

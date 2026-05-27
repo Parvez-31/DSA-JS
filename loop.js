28a4ef61341659001b88feb826632b34cbf81e05
Parvez-31 <parveztarafder31@gmail.com>
2026-05-28 00:49:29 +0530

    add pyramid pattern example to loop.js

--- loop.js
+++ loop.js
@@ -138,3 +138,12 @@
 //   }
 //   console.log(row);
 // }
+
+// let n = 4;
+// for (let i = 0; i < n; i++) {
+//   let row = " ";
+//   for (let j = 0; j < i + 1; j++) {
+//     row = row + "*";
+//   }
+//   console.log(row);
+// }

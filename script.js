1c5864ff9d848e76e743e3af520d6a7dfe582c4e
Parvez Hossien Tarafder <parveztarafder31@gmail.com>
2026-07-13 04:50:36 +0530

    add additional loop examples and a review object to script.js

--- script.js
+++ script.js
@@ -183,3 +183,30 @@
 //         console.log(i,j)
 //     }
 // }
+
+// for (let i = 0; i < 3; i++) {
+//   for (let j = i; j > 0; j--) {
+//     console.log(i, j);
+//   }
+// }
+
+// for (let i = 0; i < 3; i++) {
+//   for (let j = i; j >= 0; j--) {
+//     console.log(i, j);
+//   }
+// }
+
+// for (let i = 5; i > 0; i--) {
+//   for (let j = 0; j < i; j++) {
+//     console.log(i, j);
+//   }
+// }
+
+const review = [
+  {
+    id: 1,
+    helo: "parve",
+  },
+];
+
+console.log(review.length);

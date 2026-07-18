4c52930061623915fada3f1e5eb55649394fcd2a
Parvez Hossien Tarafder <parveztarafder31@gmail.com>
2026-07-18 09:11:46 +0530

    comment out questions 7 to 9 in star.pattern.js

--- star.pattern.js
+++ star.pattern.js
@@ -59,16 +59,48 @@
 // }
 
 // QUESTION: 7
-let n = 5;
-for (let i = 0; i < n; i++) {
-  let row = "";
-  // for empty spaces
-  for (let j = 0; j < n - (i + 1); j++) {
-    row = row + " ";
-  }
-  // for stars
-  for (let k = 0; k < i + 1; k++) {
-    row = row + "*";
-  }
-  console.log(row);
-}
+// let n = 5;
+// for (let i = 0; i < n; i++) {
+//   let row = "";
+//   // for empty spaces
+//   for (let j = 0; j < n - (i + 1); j++) {
+//     row = row + " ";
+//   }
+//   // for stars
+//   for (let k = 0; k < i + 1; k++) {
+//     row = row + "*";
+//   }
+//   console.log(row);
+// }
+
+// QUESTION: 8
+// let n = 5;
+// for (let i = 0; i < n; i++) {
+//   let row = "";
+//   let sw = 1;
+//   for (let j = 0; j < i + 1; j++) {
+//     row = row + sw;
+//     if (sw === 1) {
+//       sw = 0;
+//     } else {
+//       sw = 1;
+//     }
+//   }
+//   console.log(row);
+// }
+
+// QUESTION: 9
+// let n = 5;
+// let sw = 1;
+// for (let i = 0; i < n; i++) {
+//   let row = "";
+//   for (let j = 0; j < i + 1; j++) {
+//     row = row + sw;
+//     if (sw === 1) {
+//       sw = 0;
+//     } else {
+//       sw = 1;
+//     }
+//   }
+//   console.log(row);
+// }

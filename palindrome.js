ab39df6de27b3861addf5a5fafa007b4e25dde92
Parvez Hossien Tarafder <parveztarafder31@gmail.com>
2026-07-19 08:00:50 +0530

    add isPalindrome function to check if a number is a palindrome

---
+++ palindrome.js
@@ -0,0 +1,18 @@
+const isPalindrome = (x) => {
+  let xcopy = x;
+  let rev = 0;
+  while (x > 0) {
+    let rem = x % 10;
+    rev = 10 * rev + rem;
+    x = Math.floor(x / 10);
+  }
+
+  //   console.log(rev);
+  if (rev === xcopy) {
+    return true;
+  } else {
+    return false;
+  }
+};
+console.log(isPalindrome(121));
+// console.log(true);
